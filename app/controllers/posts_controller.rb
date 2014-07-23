106c221dab3889424a0654342fa4b3c8bc7384a5
valeriemettler <valeriemettler@gmail.com>
2014-07-23 06:03:10 +0000

    Associated Users and Posts and Added Author Info

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
   end
 
      def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
      if @post.save
        flash[:notice] = "Post was saved."
        redirect_to @post
@@ -36,4 +37,7 @@ class PostsController < ApplicationController
        render :edit
      end
    end
+
+   private
+
 end
