b32c21731783195973bee536479f06973cea0292
valeriemettler <valeriemettler@gmail.com>
2014-07-25 17:20:00 +0000

    Added Validation to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,5 +10,9 @@ class Post < ActiveRecord::Base
 
   # scope :private, -> { where(private: true) }
 
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
 
 end
