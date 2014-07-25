971e3c7b9789f0d6c30c935eca6fe31ae879db8d
valeriemettler <valeriemettler@gmail.com>
2014-07-25 01:49:37 +0000

    Configured app for Producation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,5 +30,6 @@ Bloccit::Application.configure do
    config.action_mailer.default_url_options = { host: 'localhost:3000' }
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.perform_deliveries = true
+   config.mailer_sender = "valeriemettler@gmail.com"
    
 end
