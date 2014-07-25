b91b015faa9f2e9b6573cb390ff52c48ea1fef1a
valeriemettler <valeriemettler@gmail.com>
2014-07-25 01:05:49 +0000

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,20 @@ users = User.all
 
 # The `save` method then saves this User to the database.
 
+ # Create Topics
+ 15.times do
+   Topic.create(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create(
     user:   users.sample,
+    topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -40,10 +49,35 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes(
+# Create an admin user
+ admin = User.new(
+  name:  'Admin User',
   email: 'valeriemettler@gmail.com',
   password: 'helloworld',
-)
+ role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
