696709dab5b9d425b6393fbbbd41746257e55d82
Valerie Mettler <valeriemettler@gmail.com>
2014-07-30 09:23:23 -0700

    comments working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
+    @comment = Comment.new
   end
 
   def new
