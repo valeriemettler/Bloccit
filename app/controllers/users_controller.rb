50269a540b6425864db83e394aba145978feac25
Valerie Mettler <valeriemettler@gmail.com>
2014-08-09 21:10:16 -0700

    fixed user link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,6 @@ class UsersController < ApplicationController
      end
    end
  
-   private
- 
-   def user_params
-     params.require(:user).permit(:name, :avatar, :email_favorites)
-   end
-   
    def show
      @user = User.find(params[:id])
      @posts = @user.posts.visible_to(current_user)
@@ -25,6 +19,15 @@ class UsersController < ApplicationController
    def index
      @users = User.top_rated.paginate(page: params[:page], per_page: 10)
    end
+
+
+   private
+ 
+   def user_params
+     params.require(:user).permit(:name, :avatar, :email_favorites)
+   end
+   
+   
 
 
  end
